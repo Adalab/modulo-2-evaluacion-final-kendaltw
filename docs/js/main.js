d197f62de0eef40179616680d5e77a046fa15a2d
kendaltw <kendaltellez@gmail.com>
2024-11-07 22:07:38 +0100

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,7 +1,12 @@
-const c=document.querySelector(".js-btn"),o=document.querySelector(".js-input");let i=[];const l=document.querySelector(".js-list");function a(){for(const e of i){const t=e.images.jpg.image_url;l.innerHTML+=`
+const a=document.querySelector(".js-btn"),l=document.querySelector(".js-input");let c=[],o=[];const r=document.querySelector(".js-list"),m=document.querySelector(".js-favorites");function d(){for(const e of c){const t=e.images.jpg.image_url;r.innerHTML+=`
             <li class="js-anime" id=${e.mal_id}>  
             <h5>${e.title}</h5>
             <img src="${t}" class="image" alt="${e.title} "/>
             </li>
-            `,t==="https://cdn.myanimelist.net/img/sp/icon/     apple-touch-icon-256.png"&&(t.innerHTML="https://via.placeholder.com/210x295/ffffff/666666/?text=TV.");const n=document.querySelectorAll(".js-anime");for(const s of n)s.addEventListener("click",r)}}function r(e){const t=e.currentTarget.id;i.find(n=>n.mal_id=t)}function d(e){e.preventDefault();const t=o.value;fetch(`https://api.jikan.moe/v4/anime?q=${t}`).then(n=>n.json()).then(n=>{i=n.data,a()})}c.addEventListener("click",d);
+            `,t==="https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"&&(t="https://images.app.goo.gl/z5Jyr9h7ozRkKmscA");const i=document.querySelectorAll(".js-anime");for(const n of i)n.addEventListener("click",g)}}function u(e){e.preventDefault();const t=l.value;fetch(`https://api.jikan.moe/v4/anime?q=${t}`).then(i=>i.json()).then(i=>{c=i.data,d()})}a.addEventListener("click",u);function g(e){const t=e.currentTarget.id,i=c.find(s=>s.mal_id=t);o.push(i),console.log(o),localStorage.setItem("favorites",JSON.stringify(o));const n=JSON.parse(localStorage.getItem("favorites"));for(const s of n)m.innerHTML+=`
+            <li class="js-favorites" id=${s.mal_id}>  
+            <h5>${s.title}</h5>
+            <img src="${s.images.jpg.image_url}" class="image" alt="${s.title} "/>
+            </li>
+            `}
 //# sourceMappingURL=main.js.map
