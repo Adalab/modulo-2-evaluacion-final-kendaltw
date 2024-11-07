dd7212c25c33607e6ee9950097fbe94be4fc98da
kendaltw <kendaltellez@gmail.com>
2024-11-07 11:41:06 +0100

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,2 +1,7 @@
-
+const s=document.querySelector(".js-btn"),o=document.querySelector(".js-input");let n=[];const c=document.querySelector(".js-list");function l(){for(const t of n)c.innerHTML+=`
+            <li id=${t.mal_id}>  
+            <h5>${t.title}</h5>
+            <img src="${t.images.jpg.image_url}" alt="${t.title}"/>
+            </li>
+            `}function r(t){t.preventDefault();const i=o.value;fetch(`https://api.jikan.moe/v4/anime?q=${i}`).then(e=>e.json()).then(e=>{n=e.data,l(),console.log(n)})}s.addEventListener("click",r);
 //# sourceMappingURL=main.js.map
