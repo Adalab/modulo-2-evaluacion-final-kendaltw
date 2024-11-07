792d0250ae76b66ba835ff3c7d3c5627a64aa282
kendaltw <kendaltellez@gmail.com>
2024-11-07 09:36:52 +0100

    basic structure

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+
+/* 1 Buscador de series
+    -seleccionar los elementos de mi html
+    -Cuando la usuaria hace click en el boton "buscar"
+        -recoger lo que ha escrito en el input
+        -Pedir datos al servidor
+        -filtrar la lista de series por el nombre con lo que se ha escrito
+        -acceder/guardar los datos que necesito
+        -Pintar las series en mi html filtradas
+
+*/
+
+/* 2 Favoritos
+    Cuando la usuaria haga click en una serie
+        -saber qué paleta ha clickado
+        -cambiar el estilo de esa tarjeta
+        -añadir esa serie a la lista de favoritos en localStorage
+        -pintar las series seleccionadas a la izda de la pagina
+ */
+
