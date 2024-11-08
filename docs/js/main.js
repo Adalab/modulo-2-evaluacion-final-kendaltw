a7f824ca49f0fc99e87e40ac34ce800d135fb996
kendaltw <kendaltellez@gmail.com>
2024-11-08 11:47:02 +0100

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,12 +1,12 @@
-const r=document.querySelector(".js-btn"),f=document.querySelector(".js-input");let o=[],s=[];const m=document.querySelector(".js-list");let a=document.querySelector(".js-favorites");function u(){for(const e of o){let i=e.images.jpg.image_url;i==="https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"&&(console.log("que pasa"),i="https://via.placeholder.com/210x295/ffffff/666666/?text=TV"),m.innerHTML+=`
+const d=document.querySelector(".js-btn"),c=document.querySelector(".js-input");let l=[],s=[];const r=document.querySelector(".js-list");let a=document.querySelector(".js-favorites");const f=document.querySelector(".js-reset");function m(){for(const e of l){let t=e.images.jpg.image_url;t==="https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"&&(console.log("que pasa"),t="https://via.placeholder.com/210x295/ffffff/666666/?text=TV"),r.innerHTML+=`
             <li class="js-anime" id=${e.mal_id}>  
             <h5>${e.title}</h5>
-            <img src="${i}" class="image" alt="${e.title} "/>
+            <img src="${t}" class="image" alt="${e.title} "/>
             </li>
-            `;const t=document.querySelectorAll(".js-anime");for(const n of t)n.addEventListener("click",g)}}function d(e){e.preventDefault();const i=f.value;fetch(`https://api.jikan.moe/v4/anime?q=${i}`).then(t=>t.json()).then(t=>{o=t.data,u()})}r.addEventListener("click",d);function g(e){const i=e.currentTarget.id,t=o.find(n=>n.mal_id===parseInt(i));console.log(t),s.push(t),e.currentTarget.classList.toggle("js-selected"),console.log(s),localStorage.setItem("favorites",JSON.stringify(s)),a.innerHTML="",c()}function c(){for(const e of s)a.innerHTML+=`
-            <li class="js-favorites" id=${e.mal_id}>  
-            <h5>${e.title}</h5>
-            <img src="${e.images.jpg.image_url}" class="image" alt="${e.title} "/>
+            `;const i=document.querySelectorAll(".js-anime");for(const n of i)n.addEventListener("click",h)}}function g(e){e.preventDefault();const t=c.value;fetch(`https://api.jikan.moe/v4/anime?q=${t}`).then(i=>i.json()).then(i=>{l=i.data,m()})}d.addEventListener("click",g);function h(e){const t=e.currentTarget.id,i=l.find(n=>n.mal_id===parseInt(t));console.log(i),s.push(i),console.log(s),localStorage.setItem("favorites",JSON.stringify(s)),e.currentTarget.classList.toggle("js-selected"),a.innerHTML="",u()}function u(e){for(const t of s)a.innerHTML+=`
+            <li class="js-favorites" id=${t.mal_id}>  
+            <h5>${t.title}</h5>
+            <img src="${t.images.jpg.image_url}" class="image" alt="${t.title} "/>
             </li>
-            `}const l=JSON.parse(localStorage.getItem("favorites"));l!==null&&(s=l,c());
+            `}const o=JSON.parse(localStorage.getItem("favorites"));console.log(o);o!==null&&(s=o,u());function p(e){e.preventDefault(),a.innerHTML="<h4>Favoritas</h4>",r.innerHTML="<h4>Resultados</h4>",c.value="",localStorage.clear(),s=[]}f.addEventListener("click",p);
 //# sourceMappingURL=main.js.map
